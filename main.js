8e8fe9a069ddddf570a3f40ea96afccd04092a07
celxkodez <decele2011@gmail.com>
2019-12-12 21:01:45 +0100

    bug fixed

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ recognition.interimResults = true; //return results while still working on curre
 //for the output
 let speechText = document.querySelector('#speechText');
 let out = '';
-// let textFin;
+let textFin = '';
 
 //once speech recognition determines it has a "result", grab the texts of that result, join all of them, and add to paragraph
 recognition.addEventListener("result", e => {
@@ -22,14 +22,14 @@ recognition.addEventListener("result", e => {
   
  
   if (e.results[0].isFinal) {
-   speechText.value = out;
+   textFin = out;
   }
 });
 
 function startRecording() {
   recognition.start();
   recognition.addEventListener("end", recognition.start);
-  // speakText.value = speechText.value + textFin;
+  speechText.value = speechText.value + textFin;
 
   document.getElementById("stop").addEventListener("click", stopRecording)
 
